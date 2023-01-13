8e89266af70be5560cc5d349c5c0582b7151cbd9
Talon LeFevre <talonmyers@yahoo.com>
2023-01-12 22:00:24 -0500

    working on hamburger menu

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ function goToHome() {
   }
   
  const menu_btn = document.querySelector('.hamburger');
+ const mobile_menu = document.querySelector('.mobile');
 
  menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
  });
