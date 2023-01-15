b33bb9452c09f8c465dca8a767dbaaa5f852071a
Talon LeFevre <talonmyers@yahoo.com>
2023-01-14 22:12:35 -0500

    new menu bar

--- main.js
+++ main.js
@@ -1,36 +1,8 @@
-function goToHome() {
-  window.location.href = "index.html";
-}
-
-function goToAbout() {
-  window.location.href = "about.html";
-}
-function goToServices() {
-  window.location.href = "services.html";
-} 
-function goToContact() {
-  window.location.href = "contact.html";
-}
-
-const menuBtn = document.querySelector(".menu-btn");
-const menuItems = document.querySelector(".menu-items");
-const menuItem = document.querySelectorAll(".menu-item");
-
-// main toggle
-menuBtn.addEventListener("click", () => {
-  toggle();
-});
-
-// toggle on item click if open
-menuItem.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (menuBtn.classList.contains("open")) {
-      toggle();
-    }
-  });
-});
-
-function toggle() {
-  menuBtn.classList.toggle("open");
-  menuItems.classList.toggle("open");
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
