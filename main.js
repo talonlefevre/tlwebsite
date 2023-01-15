b53e4949dec0138607b55410f6b535c80adc41ae
Talon LeFevre <talonmyers@yahoo.com>
2023-01-15 00:33:22 -0500

    nav bar changes

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+function goToHome() {
+  console.log("goToHome function called");
+  window.location.href = "index.html";
+}
+
+function goToAbout() {
+  window.location.href = "about.html";
+}
+function goToServices() {
+  window.location.href = "services.html";
+}
+function goToContact() {
+  window.location.href = "contact.html";
+}
